7f33f1456dafff21bfc25c3d0b4ea1b7b828b5f3
Jacob Lutin <jacobl1933@gmail.com>
2019-05-12 22:51:03 +0300

    complete tasks

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ App = {
       $newTaskTemplate.find('input')
                       .prop('name', taskId)
                       .prop('checked', taskCompleted)
-                      // .on('click', App.toggleCompleted)
+                      .on('click', App.toggleCompleted)
 
       // Put the task in the correct list
       if (taskCompleted) {
@@ -115,7 +115,14 @@ App = {
     await App.todoList.createTask(content)
     window.location.reload()
   },
-  
+
+  toggleCompleted: async (e) => {
+    App.setLoading(true)
+    const taskId = e.target.name
+    await App.todoList.toggleCompleted(taskId)
+    window.location.reload()
+  },
+
   setLoading: (boolean) => {
     App.loading = boolean
     const loader = $('#loader')
